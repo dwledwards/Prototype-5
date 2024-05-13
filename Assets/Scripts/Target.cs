47dde3698dd438d708e870e6462e5e5313f761a7
Dwayne Edwards <edwardsdl@jncb.com>
2024-05-13 14:53:06 -0500

    First commit

---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    private GameManager gameManager;
+    private float minForce = 12f;
+    private float maxForce = 16f;
+    private float maxTorquee = 10f;
+    private float xRange = 4f;
+    private float yRange = -3f;
+
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+
+        targetRb.AddForce (RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+
+        transform.position = RandomPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if(gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+
+        if(!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minForce, maxForce);
+    }
+
+    Vector3 RandomPosition()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), yRange);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorquee, maxTorquee);
+    }
+}
